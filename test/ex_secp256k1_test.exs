1147c310ccc6fba66f4c0e4c05ccb3e5a784af87
Ayrat Badykov <ayratin555@gmail.com>
2020-10-06 11:29:58 +0300

    add test

--- test/ex_secp256k1_test.exs
+++ test/ex_secp256k1_test.exs
@@ -75,11 +75,30 @@ defmodule ExSecp256k1Test do
     end
   end
 
-  describe "sign_compact" do
+  describe "sign_compact/2" do
+    setup do
+      private_key =
+        "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f"
+        |> String.upcase()
+        |> Base.decode16!()
+
+      message =
+        <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 2>>
+
+      {:ok, %{private_key: private_key, message: message}}
+    end
+
     test "returns signature in the compact form", %{
       private_key: private_key,
       message: message
     } do
+      assert {:ok,
+              {<<73, 102, 23, 43, 29, 88, 149, 68, 77, 65, 248, 57, 200, 155, 43, 249, 154, 95,
+                 100, 185, 121, 244, 84, 178, 159, 90, 254, 45, 27, 177, 221, 218, 21, 214, 167,
+                 20, 61, 86, 189, 86, 241, 39, 239, 70, 71, 66, 201, 140, 21, 23, 206, 201, 129,
+                 255, 24, 20, 160, 152, 36, 114, 115, 245, 33, 208>>,
+               1}} = ExSecp256k1.sign_compact(message, private_key)
     end
   end
 
